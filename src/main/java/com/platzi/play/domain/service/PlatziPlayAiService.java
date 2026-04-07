4ee1f13ef02eabd0d4b675a981431023a09407c3
Jordi Martínez <michaeljordimartinez@gmail.com>
2026-04-07 00:46:25 -0600

    add: estructura de directorios y refactor del packagin name

--- src/main/java/com/platzi/play/domain/service/PlatziPlayAiService.java
+++ src/main/java/com/platzi/play/domain/service/PlatziPlayAiService.java
@@ -1,14 +1,15 @@
-package com.platzi_play;
+package com.platzi.play.domain.service;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService
 public interface PlatziPlayAiService {
 
     @UserMessage("""
-            Genera un saludo de bienvenida a la plataforma de gestión de películas PlatziPlay. 
+            Genera un saludo de bienvenida a la plataforma de gestión de películas {{platform}}. 
             Usa menos de 120 caracteres y hazlo con un estilo amigable.
             """)
-    public String generateGreeting();
+    public String generateGreeting(@V("platform") String platform);
 }
--- src/test/java/com/platzi/play/PlatziPlayApplicationTests.java
+++ src/test/java/com/platzi/play/PlatziPlayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.platzi_play;
+package com.platzi.play;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

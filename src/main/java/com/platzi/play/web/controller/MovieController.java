da71ecc0216d172e8478ef426b42a620e1583ea4
Jordi Martínez <michaeljordimartinez@gmail.com>
2026-05-01 14:14:41 -0600

    refactor: el controlador de películas para trabajar con response entity y devolver algunos errores más específicos

--- src/main/java/com/platzi/play/web/controller/MovieController.java
+++ src/main/java/com/platzi/play/web/controller/MovieController.java
@@ -4,6 +4,7 @@ import com.platzi.play.domain.dto.MovieDto;
 import com.platzi.play.domain.service.MovieService;
 import com.platzi.play.persistence.crud.CrudMovieEntity;
 import com.platzi.play.persistence.entity.MovieEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,12 +22,20 @@ public class MovieController {
     }
 
     @GetMapping
-    public List<MovieDto> getAll() {
-        return (List<MovieDto>) this.movieService.getAll();
+    public ResponseEntity<List<MovieDto>> getAll() {
+        List<MovieDto> movieDtos = (List<MovieDto>) this.movieService.getAll();
+
+        if(movieDtos == null) return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(movieDtos);
     }
 
     @GetMapping("/{id}")
-    public MovieDto getById(@PathVariable long id){
-        return this.movieService.getById(id);
+    public ResponseEntity<MovieDto> getById(@PathVariable long id){
+        MovieDto movieDto = this.movieService.getById(id);
+
+        if(movieDto == null) return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(movieDto);
     }
 }
